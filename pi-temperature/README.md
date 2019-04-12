6b1a7ec749487590c6bda06d35ad83293613e90f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-04-12 14:14:43 +0700

    Add pi-temperature

---
+++ pi-temperature/README.md
@@ -0,0 +1,11 @@
+PI-Temperature
+
+## Dependencies
+
+* [Adafruit_Python_DHT](https://github.com/adafruit/Adafruit_Python_DHT)
+
+## Installation
+
+### Adafruit_DHT
+
+See installation of Adafruit_DHT on [Adafruit_DHT's github page](https://github.com/adafruit/Adafruit_Python_DHT).
---
+++ pi-temperature/sense.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import Adafruit_DHT
+
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT22
+
+# Example using a Raspberry Pi with DHT sensor
+# connected to GPIO23.
+pin = 23
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading. Try again!')
